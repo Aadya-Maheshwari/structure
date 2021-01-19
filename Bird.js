b3a82a88a2376e3a0622030fff16575f44db9200
Aadya-Maheshwari <76033806+Aadya-Maheshwari@users.noreply.github.com>
2021-01-19 13:02:49 +0530

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,29 @@
+class Bird {
+    constructor(x,y){
+       var option={
+           restitution:0.8,
+           friction:0.3,
+           density:1
+       } 
+        this.body=Bodies.rectangle(x,y,50,50,option);
+        this.width=50;
+        this.height=50;
+       
+        World.add(world,this.body);
+
+    }
+    display(){
+        this.body.position.x=mouseX;
+        this.body.position.y=mouseY;
+        push();
+        translate(this.body.position.x,this.body.position.y);
+        rotate(this.body.angle);
+        
+        rectMode(CENTER);
+        strokeWeight(4);
+        stroke("blue");
+        fill("red");
+        rect(0,0,this.width,this.height);
+        pop();
+    }
+}
